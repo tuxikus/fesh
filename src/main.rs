14eee6669330ad7c987408101791ad76d2e59fa4
tuxikus <contact@tuxikus.de>
2025-10-26 19:28:04 +0100

    implemented args with some weird error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-use std::{io::{stdin, stdout, Write}, process::Command};
+use std::{fmt, io::{stdin, stdout, Write}, process::Command};
+
+struct CommandInput {
+    command: String,
+    args: Vec<String>,
+}
+
+enum CommandInputError {
+    Empty,
+    UnableToParse,
+}
+
+impl fmt::Display for CommandInputError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CommandInputError::Empty => write!(f, "command input is empty"),
+            CommandInputError::UnableToParse => write!(f, "unable to parse input"),
+        }
+    }
+}
 
 struct Fesh {
     prompt: String,
@@ -17,20 +36,56 @@ impl Fesh {
         };
     }
 
-    fn read_user_input(&self) -> String {
+    fn read_user_input(&self) -> CommandInput {
         let mut input = String::new();
         if let Err(e) = stdin().read_line(&mut input) {
             eprint!("failed to read user input: {e}");
         }
-
-        input.trim().to_string()
+        
+        match self.parse_input(input.trim().to_string()) {
+            Ok(command_input) => command_input,
+            Err(e) => {
+                eprintln!("failed to parse user input: {e}");
+                return CommandInput { command: "".to_string(), args: vec![] };
+            }
+        }
     }
 
-    fn execute_command(&self, command: &String) {
-        let mut child = match Command::new(command).spawn() {
+    fn parse_input(&self, input: String) -> Result<CommandInput, CommandInputError> {
+        let cmd: String;
+        let mut args: Vec<String> = Vec::new();
+
+        if input.is_empty() {
+            return Err(CommandInputError::Empty);
+        }
+
+        // FIXME: remove go code :)
+        let parts: Vec<&str> = input.split_whitespace().collect();
+        
+        if parts.is_empty() {
+            return Err(CommandInputError::UnableToParse);
+        }
+        
+        cmd = parts[0].to_string();
+
+        for i in 1..parts.len() {
+            args.push(parts[i].to_string());
+        }
+        
+        return Ok(CommandInput { command: cmd, args: args });
+    }
+
+    fn execute_command(&self, command_input: CommandInput) {
+        let cmd = command_input.command;
+        let args = command_input.args;
+        
+        let mut child = match Command::new(&cmd)
+            .args(args)
+            .spawn()
+             {
             Ok(c) => c,
             Err(e) => {
-                eprintln!("failed to spaw child process <{command}>: {e}");
+                eprintln!("failed to spaw child process <{cmd}>: {e}");
                 return;
             }
         };
@@ -48,7 +103,7 @@ fn main() {
 
     loop {
         f.print_prompt();
-        let i = f.read_user_input();
-        f.execute_command(&i);
+        let command_input = f.read_user_input();
+        f.execute_command(command_input);
     }
 }
