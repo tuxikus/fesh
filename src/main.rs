7a88ffcc6f196c9a77ed299c774b9ec18e521d05
tuxikus <contact@tuxikus.de>
2025-10-26 15:33:58 +0100

    added fesh struct with print_prompt func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::{stdin, stdout, Write};
+
+struct Fesh {
+    prompt: String,
+}
+
+impl Fesh {
+    fn print_prompt(&self) {
+        let prompt: &[u8] = self.prompt.as_bytes();
+        if let Err(e) = stdout().write_all(prompt) {
+            eprintln!("failed to write prompt: {e}");
+            return;
+        };
+
+        if let Err(e) = stdout().flush() {
+            eprintln!("failed to flush stdout: {e}")
+        };
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f = Fesh{
+        prompt: String::from("fesh> ")
+    };
+    
+    loop {
+        f.print_prompt();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+    }
 }
