7d7ec638e7804c853b97279bbd7c86fb01cbc029
tuxikus <contact@tuxikus.de>
2025-10-26 16:00:19 +0100

    basic command execution working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, stdout, Write};
+use std::{io::{stdin, stdout, Write}, process::Command};
 
 struct Fesh {
     prompt: String,
@@ -16,17 +16,39 @@ impl Fesh {
             eprintln!("failed to flush stdout: {e}")
         };
     }
+
+    fn read_user_input(&self) -> String {
+        let mut input = String::new();
+        if let Err(e) = stdin().read_line(&mut input) {
+            eprint!("failed to read user input: {e}");
+        }
+
+        input.trim().to_string()
+    }
+
+    fn execute_command(&self, command: &String) {
+        let mut child = match Command::new(command).spawn() {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("failed to spaw child process <{command}>: {e}");
+                return;
+            }
+        };
+        
+        if let Err(e) = child.wait() {
+            eprintln!("failed to wait for child process: {e}")
+        }
+    }
 }
 
 fn main() {
-    let f = Fesh{
-        prompt: String::from("fesh> ")
+    let f = Fesh {
+        prompt: String::from("fesh> "),
     };
-    
+
     loop {
         f.print_prompt();
-
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
+        let i = f.read_user_input();
+        f.execute_command(&i);
     }
 }
